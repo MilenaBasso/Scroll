9e71c09ef2cce856be6b860d389ef86a51e27329
Milena <milena.basso444@gmail.com>
2024-04-12 14:53:16 -0300

    colors and style ready

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { ScrollView, View } from "react-native";
+import { styles } from "./Src/Styles/StylesSheet";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View> style={styles.header} </View>
-      <ScrollView horizontal={true}>
-        <View style={styles.cont}>
-          <View style={(styles.body, { backgroundColor: "red" })}></View>
-          <View style={(styles.body, { backgroundColor: "orange" })}></View>
-          <View style={(styles.body, { backgroundColor: "yellow" })}></View>
-          <View style={(styles.body, { backgroundColor: "green" })}></View>
-          <View style={(styles.body, { backgroundColor: "blue" })}></View>
-          <View style={(styles.body, { backgroundColor: "purple" })}></View>
-          <View style={(styles.body, { backgroundColor: "pink" })}></View>
-        </View>
-        <View style={styles.footer} />
+      <View style={styles.header} />
+      <ScrollView horizontal={false}>
+        <ScrollView horizontal={true}>
+          <View style={styles.cont}>
+            <View style={[styles.body, { backgroundColor: "maroon" }]}></View>
+            <View style={[styles.body, { backgroundColor: "red" }]}></View>
+            <View style={[styles.body, { backgroundColor: "crimson" }]}></View>
+            <View style={[styles.body, { backgroundColor: "orange" }]}></View>
+            <View style={[styles.body, { backgroundColor: "#FFA07A" }]}></View>
+            <View style={[styles.body, { backgroundColor: "#FFFFE0" }]}></View>
+            <View style={[styles.body, { backgroundColor: "yellow" }]}></View>
+            <View style={[styles.body, { backgroundColor: "#90EE90" }]}></View>
+            <View style={[styles.body, { backgroundColor: "green" }]}></View>
+            <View style={[styles.body, { backgroundColor: "blue" }]}></View>
+            <View style={[styles.body, { backgroundColor: "#9370DB" }]}></View>
+            <View style={[styles.body, { backgroundColor: "purple" }]}></View>
+            <View style={[styles.body, { backgroundColor: "#E6E6FA" }]}></View> 
+            <View style={[styles.body, { backgroundColor: "#E6E6FA" }]}></View>
+            <View style={[styles.body, { backgroundColor: "pink" }]}></View>
+          </View>
+          <View style={styles.footer} />
+        </ScrollView>
       </ScrollView>
     </View>
   );
--- Src/Styles/StylesSheet.js
+++ Src/Styles/StylesSheet.js
@@ -3,20 +3,30 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
   },
 
   header: {
-
+    height: 30,
+    backgroundColor: "gray",
   },
 
   body: {
+    height: 800,
+    width: 600,
+    padding: 20,
+    margin: 20,
+  },
 
+  cont: {
+    flex: 1,
+    flexDirection: "row",
+    padding: 20,
+    margin: 10,
   },
 
   footer: {
-    
+    height: 40,
+    backgroundColor: "black",
   },
 });
